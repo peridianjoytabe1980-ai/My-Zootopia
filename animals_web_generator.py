7728b838477e366556b62bbac2458ac4424925b1
Peridian Joy Tabe <Peridianjoytabe1980@gmail.com>
2025-10-15 23:31:28 +0200

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -19,12 +19,12 @@ for animal in animals:
     animal_type = animal.get("characteristics", {}).get("type")
     locations = ", ".join(animal.get("locations", []))
 
-    output += f"Name: {name}\n"
-    output += f"Diet: {diet}\n"
-    output += f"Location: {locations}\n"
+    output += '<li class="cards__item">'
+    output += f"Name: {animal['name']}<br/>\n"
+    output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
     if animal_type:  # Only print if type exists
         output += f"Type: {animal_type}\n"
-    output += "\n"
+    output += '</li>'
 
 
 new_html = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
