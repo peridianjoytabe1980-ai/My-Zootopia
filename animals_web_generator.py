08e0abd9bb74289a9ca0c2ff92881d242b87c6c4
Peridian Joy Tabe <Peridianjoytabe1980@gmail.com>
2025-10-15 23:23:45 +0200

    Bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -4,7 +4,14 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
+
+with open("animals_template.html", "r") as file:
+    template_content = file.read()
+
 animals = load_data("animals_data.json")
+
+output = ""
+
 
 for animal in animals:
     name = animal.get("name", "Unknown")
@@ -12,9 +19,22 @@ for animal in animals:
     animal_type = animal.get("characteristics", {}).get("type")
     locations = ", ".join(animal.get("locations", []))
 
-    print(f"Name: {name}")
-    print(f"Diet: {diet}")
-    print(f"Locations: {locations}")
+    output += f"Name: {name}\n"
+    output += f"Diet: {diet}\n"
+    output += f"Location: {locations}\n"
     if animal_type:  # Only print if type exists
-        print(f"Type: {animal_type}")
-    print()
+        output += f"Type: {animal_type}\n"
+    output += "\n"
+
+
+new_html = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
+
+with open("animals.html", "w") as file:
+    file.write(new_html)
+
+
+
+
+
+
+
